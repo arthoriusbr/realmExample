65f7caba12f948ae45c5ca0c721edf4387472003
administrador <administrador@Mac-mini-de-administrador.local>
2018-07-13 12:22:09 -0300

    Criando a tela de login do Unifor Mobile - Ainda sem realm

--- RealmExample/ViewController.swift
+++ RealmExample/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var logoUnifor: UIImageView!
+    @IBOutlet weak var matriculaTextBox: UITextField!
+    @IBOutlet weak var senhaTextBox: UITextField!
+    @IBOutlet weak var botaoEntrar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "fundoUnifor")!)
+        //self.view.addBackground()
+        self.logoUnifor.tintColor = .white
+        self.botaoEntrar.setTitle("Entrar", for: .normal)
+        self.botaoEntrar.layer.cornerRadius = self.botaoEntrar.frame.height / 2
+        self.matriculaTextBox.placeholder = "Digite aqui sua matrícula"
+        self.senhaTextBox.placeholder = "Digite aqui sua senha"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+//    extension UIView {
+//        func addBackground() {
+//            // screen width and height:
+//            let width = UIScreen.mainScreen().bounds.size.width
+//            let height = UIScreen.mainScreen().bounds.size.height
+//
+//            let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
+//            imageViewBackground.image = UIImage(named: "fundoUnifor")
+//
+//            // you can change the content mode:
+//            imageViewBackground.contentMode = UIViewContentMode.ScaleAspectFill
+//
+//            self.addSubview(imageViewBackground)
+//            self.sendSubviewToBack(imageViewBackground)
+//        }}
+    
+    
+    @IBAction func cliqueBotao(_ sender: Any) {
+        matriculaTextBox.text = "Oi!"
     }
-
-
+    
 }
 
